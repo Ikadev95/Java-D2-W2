85e4afd7aa58d6223c8fc36038a4e3cc7965c831
Ikadev95 <fegughi@gmail.com>
2024-11-26 16:02:33 +0100

    rubrica

--- src/esercizi/esercizio3/Rubrica.java
+++ src/esercizi/esercizio3/Rubrica.java
@@ -19,4 +19,47 @@ public class Rubrica {
         contatti.remove(nome);
         System.out.println("Contatto rimosso: " + nome);
     }
+
+    public void ricercaDaNumero(String telefono) {
+        for (String nome : contatti.keySet()) {
+            if (contatti.get(nome).equals(telefono)) {
+                System.out.println( "La persona associata al numero " + telefono + " è: " + nome);
+            }
+        }
+    }
+
+    public void cercaNumeroPerNome(String nome) {
+        if (contatti.containsKey(nome)) {
+            System.out.println("Il numero di " + nome + " è: " + contatti.get(nome));
+        }
+    }
+
+    public void stampaContatti() {
+        if (contatti.isEmpty()) {
+            System.out.println("La rubrica è vuota");
+        } else {
+            System.out.println("Elenco contatti:");
+            for (Map.Entry<String, String> entry : contatti.entrySet()) {
+                System.out.println(entry.getKey() + " -> " + entry.getValue());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Rubrica rubrica = new Rubrica();
+
+        rubrica.aggiungiContatto("Mario Rossi", "1234567890");
+        rubrica.aggiungiContatto("Luigi Verdi", "9876543210");
+        rubrica.aggiungiContatto("Anna Bianchi", "4561237890");
+
+        rubrica.stampaContatti();
+
+        rubrica.cercaNumeroPerNome("Luigi Verdi");
+
+        rubrica.ricercaDaNumero("4561237890");
+
+        rubrica.cancellaContatto("Mario Rossi");
+
+        rubrica.stampaContatti();
+    }
 }
